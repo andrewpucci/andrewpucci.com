a5e2cde91fd42d1e6a1629383c3a6a99b2c0b8d7
Andrew Pucci <andrewpucci@users.noreply.github.com>
2017-05-24 18:16:15 -0400

    Add carousel of portfolio work

--- js/init.js
+++ js/init.js
@@ -3,6 +3,7 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.carousel').carousel({dist: -30, padding: 20});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
