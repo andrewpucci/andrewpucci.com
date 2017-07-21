4e626203795c68c9a39a20de7f736b5229609a93
Andrew Pucci <andrew@andrewpucci.com>
2017-07-21 13:04:05 -0400

    Add before and after images

--- js/init.js
+++ js/init.js
@@ -5,5 +5,6 @@
     $('.parallax').parallax();
     $('.carousel.carousel-slider').carousel({fullWidth: true, indicators: true});
     $('.carousel.portfolio-overview').carousel({dist: -30, padding: 20});
+    $('.materialboxed').materialbox();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
