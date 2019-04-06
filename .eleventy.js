2fef6481b93241b615b0da55ea666ed46bd6eb7f
Andrew Pucci <andrewpucci@users.noreply.github.com>
2019-04-05 19:08:22 -0700

    Update config

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,24 @@
 module.exports = function(config) {
   // Add some utility filters
-  config.addFilter("squash", require("./src/filters/squash.js") );
+  config.addFilter('squash', require('./src/filters/squash.js') );
 
   // minify the html output
-  config.addTransform("htmlmin", require("./src/utils/minify-html.js"));
+  config.addTransform('htmlmin', require('./src/utils/minify-html.js'));
 
   // pass some assets right through
-  config.addPassthroughCopy("./src/site/assets");
-  config.addPassthroughCopy("./src/site/humans.txt");
-  config.addPassthroughCopy("./src/site/robots.txt");
+  config.addPassthroughCopy('./src/site/assets');
+  config.addPassthroughCopy('./src/site/humans.txt');
+  config.addPassthroughCopy('./src/site/robots.txt');
 
   return {
     dir: {
-      input: "src/site",
-      output: "dist",
-      data: '_data'
+      input: 'src/site',
+      layouts: '_layouts',
+      output: 'dist',
+      data: '_data',
     },
-    templateFormats : ["njk", "md"],
-    htmlTemplateEngine : "njk",
-    markdownTemplateEngine : "njk",
+    templateFormats : ['njk', 'md'],
+    htmlTemplateEngine : 'njk',
+    markdownTemplateEngine : 'njk',
   };
 };
