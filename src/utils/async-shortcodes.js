cecea18982e39fe0bfccd90d41d75d5dfd5c4dba
Andrew Pucci <andrewpucci@users.noreply.github.com>
2021-07-08 18:49:08 -0400

    Add card shortcode

--- src/utils/async-shortcodes.js
+++ src/utils/async-shortcodes.js
@@ -6,4 +6,19 @@ module.exports = {
     return img.image(imgSrc, imgAlt, imgCls, imgSizes, imgWidths);
   },
 
+  async card(cardTitle, cardContent, cardURL, imgSrc, imgAlt, imgCls, imgSizes, imgWidths) {
+    const cardImg = await img.image(imgSrc, imgAlt, imgCls, imgSizes, imgWidths);
+    return `<div class="card">
+        ${cardImg}
+        <div class="card-body">
+          <p class="h5 card-title">${cardTitle}</p>
+          <p class="card-text">${cardContent}</p>
+        </div>
+        <div class="card-footer bg-white border-top-0">
+          <a href="${cardURL}" class="card-link stretched-link" aria-label="Read more about ${cardTitle}">Read more</a>
+        </div>
+      </div>
+    </div>`;
+  },
+
 };
