9351bec22adfc42ff86e5e91e8a0119d69f7d55d
Andrew Pucci <andrewpucci@users.noreply.github.com>
2021-07-08 18:49:29 -0400

    Add expandableImage shortcode

--- src/utils/async-shortcodes.js
+++ src/utils/async-shortcodes.js
@@ -21,4 +21,27 @@ module.exports = {
     </div>`;
   },
 
+  async expandableImage(imgName, imgSrc, imgAlt) {
+    const thumbImage = await img.image(imgSrc, imgAlt, 'img-fluid img-thumbnail', '629', [629, 1258, null]);
+    const fullImage = await img.image(imgSrc, '', 'img-fluid');
+
+    return `<a href="#${imgName}-modal" data-bs-toggle="modal">
+        ${thumbImage}
+      </a>
+      <div class="modal fade" id="${imgName}-modal" tabindex="-1" aria-labelledby="${imgName}-modal-title" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="${imgName}-modal-title">${imgAlt}</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ${fullImage}
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  },
+
 };
