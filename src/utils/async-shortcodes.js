30d2e76adc9b47792b95f9660e4ccc2581a31ee5
Andrew Pucci <andrewpucci@users.noreply.github.com>
2025-07-09 00:32:35 -0400

    feat: upgrade 11ty

--- src/utils/async-shortcodes.js
+++ src/utils/async-shortcodes.js
@@ -1,47 +1,48 @@
-const img = require('./image.js');
+import * as img from './image.js';
 
-module.exports = {
+export const image = async (imgSrc, imgAlt, imgCls, imgSizes, imgWidths) => {
+  return img.image(imgSrc, imgAlt, imgCls, imgSizes, imgWidths);
+};
 
-  async image(imgSrc, imgAlt, imgCls, imgSizes, imgWidths) {
-    return img.image(imgSrc, imgAlt, imgCls, imgSizes, imgWidths);
-  },
+export const card = async (cardTitle, cardContent, cardURL, imgSrc, imgAlt, imgCls, imgSizes, imgWidths) => {
+  const cardImg = await img.image(imgSrc, imgAlt, imgCls, imgSizes, imgWidths);
+  return `<div class="card">
+    ${cardImg}
+    <div class="card-body">
+      <p class="h5 card-title">${cardTitle}</p>
+      <p class="card-text">${cardContent}</p>
+    </div>
+    <div class="card-footer bg-white border-top-0">
+      <a href="${cardURL}" class="card-link stretched-link" aria-label="Read more about ${cardTitle}">Read more</a>
+    </div>
+  </div>`;
+};
 
-  async card(cardTitle, cardContent, cardURL, imgSrc, imgAlt, imgCls, imgSizes, imgWidths) {
-    const cardImg = await img.image(imgSrc, imgAlt, imgCls, imgSizes, imgWidths);
-    return `<div class="card">
-        ${cardImg}
-        <div class="card-body">
-          <p class="h5 card-title">${cardTitle}</p>
-          <p class="card-text">${cardContent}</p>
-        </div>
-        <div class="card-footer bg-white border-top-0">
-          <a href="${cardURL}" class="card-link stretched-link" aria-label="Read more about ${cardTitle}">Read more</a>
-        </div>
-      </div>
-    </div>`;
-  },
+export const expandableImage = async (imgName, imgSrc, imgAlt) => {
+  const thumbImage = await img.image(imgSrc, imgAlt, 'img-fluid img-thumbnail', '629', [629, 1258, null]);
+  const fullImage = await img.image(imgSrc, '', 'img-fluid');
 
-  async expandableImage(imgName, imgSrc, imgAlt) {
-    const thumbImage = await img.image(imgSrc, imgAlt, 'img-fluid img-thumbnail', '629', [629, 1258, null]);
-    const fullImage = await img.image(imgSrc, '', 'img-fluid');
-
-    return `<a href="#${imgName}-modal" data-bs-toggle="modal">
-        ${thumbImage}
-      </a>
-      <div class="modal fade" id="${imgName}-modal" tabindex="-1" aria-labelledby="${imgName}-modal-title" aria-hidden="true">
-        <div class="modal-dialog modal-fullscreen">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="${imgName}-modal-title">${imgAlt}</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              ${fullImage}
-            </div>
+  return `<a href="#${imgName}-modal" data-bs-toggle="modal">
+      ${thumbImage}
+    </a>
+    <div class="modal fade" id="${imgName}-modal" tabindex="-1" aria-labelledby="${imgName}-modal-title" aria-hidden="true">
+      <div class="modal-dialog modal-fullscreen">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="${imgName}-modal-title">${imgAlt}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ${fullImage}
           </div>
         </div>
       </div>
-    `;
-  },
+    </div>`;
+};
 
+// For backward compatibility
+export default {
+  image,
+  card,
+  expandableImage
 };
--- src/utils/image.js
+++ src/utils/image.js
@@ -1,32 +1,33 @@
-const Image = require('@11ty/eleventy-img');
+import Image from '@11ty/eleventy-img';
 
-module.exports = {
+// Perform image transformations
+export const image = async (src, alt, cls, sizes = '100vw', widths = [null]) => {
 
-  // Perform image transformations
-  async image(src, alt, cls, sizes = '100vw', widths = [null]) {
-    if (alt === undefined) {
-      throw new Error(`Missing \`alt\` on responsive image from: ${src}`);
-    }
+  if (alt === undefined) {
+    throw new Error(`Missing \`alt\` on responsive image from: ${src}`);
+  }
 
-    const metadata = await Image(src, {
-      widths,
-      formats: ['webp', 'jpeg'],
-      urlPath: '/assets/img/',
-      outputDir: './dist/assets/img/',
-    });
+  const metadata = await Image(src, {
+    widths,
+    formats: ['webp', 'jpeg'],
+    urlPath: '/assets/img/',
+    outputDir: './dist/assets/img/',
+  });
 
-    const lowsrc = metadata.jpeg[0];
+  const lowsrc = metadata.jpeg[0];
 
-    return `<picture>
-      ${Object.values(metadata).map((imageFormat) => `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map((entry) => entry.srcset).join(', ')}" sizes="${sizes}">`).join('\n')}
-        <img
-          src="${lowsrc.url}"
-          width="${lowsrc.width}"
-          height="${lowsrc.height}"
-          alt="${alt}"
-          class="${cls}"
-          loading="lazy"
-          decoding="async">
-      </picture>`;
-  },
+  return `<picture>
+    ${Object.values(metadata).map((imageFormat) => `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map((entry) => entry.srcset).join(', ')}" sizes="${sizes}">`).join('\n')}
+      <img
+        src="${lowsrc.url}"
+        width="${lowsrc.width}"
+        height="${lowsrc.height}"
+        alt="${alt}"
+        class="${cls}"
+        loading="lazy"
+        decoding="async">
+    </picture>`;
 };
+
+// For backward compatibility
+export default { image };
